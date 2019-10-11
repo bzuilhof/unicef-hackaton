8b26397340841c00f5f9743481d3f96498414eef
Joris van Willigen <joris.van.willigen@navara.nl>
2019-10-11 12:48:24 +0200

    feature: earth page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EarthComponent} from './pages/earth/earth.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'earth', component: EarthComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
