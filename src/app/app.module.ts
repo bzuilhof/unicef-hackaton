7901e486af09f8df71103fc169c584fa02034812
Zuilhof <bart.zuilhof@navara.nl>
2019-10-11 12:23:01 +0200

    Added initial pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LevelSelectionComponent } from './pages/level-selection/level-selection.component';
+import { StageComponent } from './pages/stage/stage.component';
+import { EarthComponent } from './pages/earth/earth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LevelSelectionComponent,
+    StageComponent,
+    EarthComponent
   ],
   imports: [
     BrowserModule,
