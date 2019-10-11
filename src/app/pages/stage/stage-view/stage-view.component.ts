84af46ff7b272fb54b052256e696d4a3d98c88fa
Joris van Willigen <joris.van.willigen@navara.nl>
2019-10-11 23:15:40 +0200

    feature: add sounds

--- src/app/pages/stage/stage-view/stage-view.component.ts
+++ src/app/pages/stage/stage-view/stage-view.component.ts
@@ -18,11 +18,16 @@ export class StageViewComponent implements OnInit {
   shake: any;
   assetImage = 'waterput.png';
   playAnimation = 1;
+  drop1: any;
+  drop2: any;
+  drop3: any;
+  drop4: any;
 
   @Output() increase: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
   ngOnInit() {
+    this.loadAudio();
   }
 
   increaseCurrency() {
@@ -31,5 +36,36 @@ export class StageViewComponent implements OnInit {
     if (this.playAnimation === 4) {
       this.playAnimation = 1;
     }
+    this.playRandomAudio();
+  }
+
+  private playRandomAudio() {
+    const randomNum = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    switch (randomNum) {
+      case 1:
+        this.drop1.play();
+        break;
+      case 2:
+        this.drop2.play();
+        break;
+      case 3:
+        this.drop3.play();
+        break;
+      case 4:
+        this.drop4.play();
+        break;
+    }
+
+  }
+
+  private loadAudio() {
+    this.drop1 = new Audio();
+    this.drop2 = new Audio();
+    this.drop3 = new Audio();
+    this.drop4 = new Audio();
+    this.drop1.src = 'assets/sounds/drop1.wav';
+    this.drop2.src = 'assets/sounds/drop2.wav';
+    this.drop3.src = 'assets/sounds/drop3.wav';
+    this.drop4.src = 'assets/sounds/drop4.wav';
   }
 }
